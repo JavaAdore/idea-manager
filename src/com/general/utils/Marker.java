fed3f18334e89c7a2dcddf2b218f57bb30f487e1
Mahmoud Eltaieb <mahmoud.eltaieb@gmail.com>
2015-01-20 22:02:16 +0200

    first commit

---
+++ src/com/general/utils/Marker.java
@@ -0,0 +1,10 @@
+package com.general.utils;
+
+import java.io.Serializable;
+
+public abstract interface Marker extends  Serializable{
+	
+	Long getID();
+	void setID(Long ID);
+	
+}
---
+++ src/com/general/utils/SpringUtils.java
@@ -0,0 +1,18 @@
+package com.general.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.TreeMap;
+
+
+
+public class SpringUtils {
+
+	
+}
